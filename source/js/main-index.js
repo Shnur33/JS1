00eb8289707d3b7be6f52d88c9d985988b2359e0
Юрец <yrok.03.11.1989@gmai.com>
2023-11-10 18:08:11 +1000

    function

--- source/js/main-index.js
+++ source/js/main-index.js
@@ -3,14 +3,49 @@ const getRandomNumber = function (min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+const id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const idPhoto = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25,
+const avatar = [
+  "img/avatar-buildimgavatar-1.svg.svg",
+  "img/avatar-buildimgavatar-2.svg.svg",
+  "img/avatar-3.svg",
+  "img/avatar-4.svg",
+  "img/avatar-5.svg",
+  "img/avatar-6.svg",
 ];
-const url = "photo /i.jpg";
 
-const discription = [
+const message = [
+  "Все отлично!",
+  "В целом все не плохо.Но не все.",
+  "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?",
+  "Когда делаете фотографии, хорошо бы убирать палец из кадра.",
+];
+const names = [
+  "Александр",
+  "Женек",
+  "Анна",
+  "Антон",
+  "Ника",
+  "Андрюха",
+  "Денис",
+];
+const createComments = () => {
+  const randomId = getRandomNumber(0, id.length - 1);
+  const randomAvatar = getRandomNumber(0, avatar.length - 1);
+  const randomMessage = getRandomNumber(0, message.length - 1);
+  const randomName = getRandomNumber(0, names.length - 1);
+
+  return {
+    id: id[randomId],
+    avatar: avatar[randomAvatar],
+    message: message[randomMessage],
+    name: names[randomName],
+  };
+};
+
+console.log(createComments());
+
+const description = [
   "Описание придумайте самостоятельно1",
   "Описание придумайте самостоятельно2",
   "Описание придумайте самостоятельно3",
@@ -39,58 +74,17 @@ const discription = [
 ];
 
 const createPhotos = () => {
-  randomDiscription = getRandomNumber(0, discription.length - 1);
-  randomLikes = getRandomNumber(15, 200);
+  const randomDiscription = getRandomNumber(0, description.length - 1);
+  const randomLikes = getRandomNumber(15, 200);
   return {
-    idPhoto: idPhoto[randomidPhoto],
-    discription: discription[randomDiscription],
+    idPhoto: getRandomNumber(1, 25),
+    description: description[randomDiscription],
     likes: randomLikes,
+    url: `photo/${getRandomNumber(1, 25)}.jpg`,
+    comments: createComments(),
   };
 };
 const usersPhoto = new Array(25).fill(null).map(() => createPhotos());
 console.log(usersPhoto);
-
-const id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-const avatar = [
-  "img/avatar-buildimgavatar-1.svg.svg",
-  "img/avatar-buildimgavatar-2.svg.svg",
-  "img/avatar-3.svg",
-  "img/avatar-4.svg",
-  "img/avatar-5.svg",
-  "img/avatar-6.svg",
-];
-
-const message = [
-  "Все отлично!",
-  "В целом все не плохо.Но не все.",
-  "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?",
-  "Когда делаете фотографии, хорошо бы убирать палец из кадра.",
-];
-const names = [
-  "Александр",
-  "Женек",
-  "Анна",
-  "Антон",
-  "Ника",
-  "Андрюха",
-  "Денис",
-];
-
-const createComments = () => {
-  const randomId = getRandomNumber(0, id.length - 1);
-  const randomAvatar = getRandomNumber(0, avatar.length - 1);
-  const randomMessage = getRandomNumber(0, message.length - 1);
-  const randomName = getRandomNumber(0, names.length - 1);
-
-  return {
-    id: id[randomId],
-    avatar: avatar[randomAvatar],
-    message: message[randomMessage],
-    name: names[randomName],
-  };
-};
-
-console.log(createComments());
 
 //export { createComments };
